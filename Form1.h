513312480550b9f14e49d5b167e2de54e5c89337
arekako <arek.peplinski@wp.pl>
2015-06-15 16:52:25 +0200

    przedMarkupem

--- Form1.h
+++ Form1.h
@@ -11,6 +11,15 @@ namespace WindowsFormApplication1 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 	/// <summary>
+	int domyslnaplansza[9][9] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
 	int Sudoku[9][9] = { { 0, 0, 5, 0, 0, 0, 3, 0, 0 },
 	{ 0, 0, 0, 1, 0, 0, 0, 6, 0 },
 	{ 0, 0, 0, 9, 6, 0, 4, 0, 7 },
@@ -68,11 +77,20 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::Button^  button1;
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::DataGridView^  dataGridView2;
+
+
+
+
+	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column10;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column11;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column12;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column13;
-	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column14;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column15;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column16;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column17;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column18;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column2;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column3;
@@ -82,11 +100,8 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column7;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column8;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column9;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column14;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column15;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column16;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column17;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column18;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -100,16 +115,16 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle3 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle4 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle5 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle6 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle7 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle8 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle9 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
-			this->Column10 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column11 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column12 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column13 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
@@ -119,11 +134,21 @@ namespace WindowsFormApplication1 {
 			this->Column7 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column8 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column9 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->Column10 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column11 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column12 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column13 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column14 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column15 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column16 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column17 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column18 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
 			this->SuspendLayout();
@@ -145,11 +170,101 @@ namespace WindowsFormApplication1 {
 			this->dataGridView1->Size = System::Drawing::Size(290, 210);
 			this->dataGridView1->TabIndex = 0;
 			// 
+			// Column1
+			// 
+			dataGridViewCellStyle1->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column1->DefaultCellStyle = dataGridViewCellStyle1;
+			this->Column1->HeaderText = L"";
+			this->Column1->Name = L"Column1";
+			this->Column1->Width = 30;
+			// 
+			// Column2
+			// 
+			dataGridViewCellStyle2->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column2->DefaultCellStyle = dataGridViewCellStyle2;
+			this->Column2->HeaderText = L"";
+			this->Column2->Name = L"Column2";
+			this->Column2->Width = 30;
+			// 
+			// Column3
+			// 
+			dataGridViewCellStyle3->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle3->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column3->DefaultCellStyle = dataGridViewCellStyle3;
+			this->Column3->HeaderText = L"";
+			this->Column3->Name = L"Column3";
+			this->Column3->Width = 30;
+			// 
+			// Column4
+			// 
+			dataGridViewCellStyle4->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle4->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column4->DefaultCellStyle = dataGridViewCellStyle4;
+			this->Column4->HeaderText = L"";
+			this->Column4->Name = L"Column4";
+			this->Column4->Width = 30;
+			// 
+			// Column5
+			// 
+			dataGridViewCellStyle5->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle5->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column5->DefaultCellStyle = dataGridViewCellStyle5;
+			this->Column5->HeaderText = L"";
+			this->Column5->Name = L"Column5";
+			this->Column5->Width = 30;
+			// 
+			// Column6
+			// 
+			dataGridViewCellStyle6->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle6->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column6->DefaultCellStyle = dataGridViewCellStyle6;
+			this->Column6->HeaderText = L"";
+			this->Column6->Name = L"Column6";
+			this->Column6->Width = 30;
+			// 
+			// Column7
+			// 
+			dataGridViewCellStyle7->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle7->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column7->DefaultCellStyle = dataGridViewCellStyle7;
+			this->Column7->HeaderText = L"";
+			this->Column7->Name = L"Column7";
+			this->Column7->Width = 30;
+			// 
+			// Column8
+			// 
+			dataGridViewCellStyle8->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle8->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column8->DefaultCellStyle = dataGridViewCellStyle8;
+			this->Column8->HeaderText = L"";
+			this->Column8->Name = L"Column8";
+			this->Column8->Width = 30;
+			// 
+			// Column9
+			// 
+			dataGridViewCellStyle9->BackColor = System::Drawing::Color::Yellow;
+			dataGridViewCellStyle9->Font = (gcnew System::Drawing::Font(L"Lucida Calligraphy", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Column9->DefaultCellStyle = dataGridViewCellStyle9;
+			this->Column9->HeaderText = L"";
+			this->Column9->Name = L"Column9";
+			this->Column9->Width = 30;
+			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(332, 12);
+			this->button1->Location = System::Drawing::Point(311, 12);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(173, 23);
+			this->button1->Size = System::Drawing::Size(173, 51);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"£aduj planszê Sudoku";
 			this->button1->UseVisualStyleBackColor = true;
@@ -157,9 +272,9 @@ namespace WindowsFormApplication1 {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(527, 12);
+			this->button2->Location = System::Drawing::Point(678, 12);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(173, 39);
+			this->button2->Size = System::Drawing::Size(173, 47);
 			this->button2->TabIndex = 2;
 			this->button2->Text = L"Zapisz Planszê";
 			this->button2->UseVisualStyleBackColor = true;
@@ -176,124 +291,98 @@ namespace WindowsFormApplication1 {
 			});
 			this->dataGridView2->Location = System::Drawing::Point(12, 240);
 			this->dataGridView2->Name = L"dataGridView2";
-			this->dataGridView2->Size = System::Drawing::Size(753, 256);
+			this->dataGridView2->Size = System::Drawing::Size(719, 220);
 			this->dataGridView2->TabIndex = 3;
 			// 
 			// Column10
 			// 
-			this->Column10->HeaderText = L"Column10";
+			this->Column10->HeaderText = L"";
 			this->Column10->Name = L"Column10";
+			this->Column10->Width = 75;
 			// 
 			// Column11
 			// 
-			this->Column11->HeaderText = L"Column11";
+			this->Column11->HeaderText = L"";
 			this->Column11->Name = L"Column11";
+			this->Column11->Width = 75;
 			// 
 			// Column12
 			// 
-			this->Column12->HeaderText = L"Column12";
+			this->Column12->HeaderText = L"";
 			this->Column12->Name = L"Column12";
+			this->Column12->Width = 75;
 			// 
 			// Column13
 			// 
-			this->Column13->HeaderText = L"Column13";
+			this->Column13->HeaderText = L"";
 			this->Column13->Name = L"Column13";
+			this->Column13->Width = 75;
+			// 
+			// Column14
+			// 
+			this->Column14->HeaderText = L"";
+			this->Column14->Name = L"Column14";
+			this->Column14->Width = 75;
+			// 
+			// Column15
+			// 
+			this->Column15->HeaderText = L"";
+			this->Column15->Name = L"Column15";
+			this->Column15->Width = 75;
+			// 
+			// Column16
+			// 
+			this->Column16->HeaderText = L"";
+			this->Column16->Name = L"Column16";
+			this->Column16->Width = 75;
+			// 
+			// Column17
+			// 
+			this->Column17->HeaderText = L"";
+			this->Column17->Name = L"Column17";
+			this->Column17->Width = 75;
+			// 
+			// Column18
+			// 
+			this->Column18->HeaderText = L"";
+			this->Column18->Name = L"Column18";
+			this->Column18->Width = 75;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(524, 69);
+			this->label1->Location = System::Drawing::Point(729, 76);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(63, 13);
 			this->label1->TabIndex = 4;
 			this->label1->Text = L"Element 0 0";
 			// 
-			// Column1
+			// label2
 			// 
-			dataGridViewCellStyle4->BackColor = System::Drawing::Color::Yellow;
-			this->Column1->DefaultCellStyle = dataGridViewCellStyle4;
-			this->Column1->HeaderText = L"";
-			this->Column1->Name = L"Column1";
-			this->Column1->Width = 30;
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(504, 33);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(158, 13);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Po wprowadzeniu liczb kliknij w:";
 			// 
-			// Column2
+			// label3
 			// 
-			this->Column2->HeaderText = L"";
-			this->Column2->Name = L"Column2";
-			this->Column2->Width = 30;
-			// 
-			// Column3
-			// 
-			this->Column3->HeaderText = L"";
-			this->Column3->Name = L"Column3";
-			this->Column3->Width = 30;
-			// 
-			// Column4
-			// 
-			this->Column4->HeaderText = L"";
-			this->Column4->Name = L"Column4";
-			this->Column4->Width = 30;
-			// 
-			// Column5
-			// 
-			this->Column5->HeaderText = L"";
-			this->Column5->Name = L"Column5";
-			this->Column5->Width = 30;
-			// 
-			// Column6
-			// 
-			this->Column6->HeaderText = L"";
-			this->Column6->Name = L"Column6";
-			this->Column6->Width = 30;
-			// 
-			// Column7
-			// 
-			this->Column7->HeaderText = L"";
-			this->Column7->Name = L"Column7";
-			this->Column7->Width = 30;
-			// 
-			// Column8
-			// 
-			this->Column8->HeaderText = L"";
-			this->Column8->Name = L"Column8";
-			this->Column8->Width = 30;
-			// 
-			// Column9
-			// 
-			this->Column9->HeaderText = L"";
-			this->Column9->Name = L"Column9";
-			this->Column9->Width = 30;
-			// 
-			// Column14
-			// 
-			this->Column14->HeaderText = L"Column14";
-			this->Column14->Name = L"Column14";
-			// 
-			// Column15
-			// 
-			this->Column15->HeaderText = L"Column15";
-			this->Column15->Name = L"Column15";
-			// 
-			// Column16
-			// 
-			this->Column16->HeaderText = L"Column16";
-			this->Column16->Name = L"Column16";
-			// 
-			// Column17
-			// 
-			this->Column17->HeaderText = L"Column17";
-			this->Column17->Name = L"Column17";
-			// 
-			// Column18
-			// 
-			this->Column18->HeaderText = L"Column18";
-			this->Column18->Name = L"Column18";
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(324, 101);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(454, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Pamietaj ¿eby wprowadzaæ wartoœci tylko z list potencjalnych wartosci!!!Potrzebuj"
+				L"esz pomocy \?";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(863, 508);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->dataGridView2);
 			this->Controls->Add(this->button2);
@@ -329,14 +418,19 @@ namespace WindowsFormApplication1 {
 		{
 			for (System::Int32 i = 0; i < 9; i++)
 			{
-				dataGridView1->Rows[j]->Cells[i]->Value = Sudoku[j][i];
+				dataGridView1->Rows[j]->Cells[i]->Value = domyslnaplansza[j][i];//Sudoku[j][i]
+				//if (dataGridView1->Rows[j]->Cells[i]->Value == 0)
+				//{
+				//	dataGridView1->Rows[j]->Cells[i]->Style->ForeColor.Chocolate;
+				//}
+			
 			}
 		}
-		for (System::Int32 j = 0; j < 4; j++)
+		for (System::Int32 j = 0; j < 9; j++)
 		{
-			for (System::Int32 i = 0; i < 4; i++)
+			for (System::Int32 i = 0; i < 9; i++)
 			{
-				dataGridView2->Rows[j]->Cells[i]->Value = Sudoku[j][i];
+				dataGridView2->Rows[j]->Cells[i]->Value = domyslnaplansza[j][i];
 			}
 		}
 	}
